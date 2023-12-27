ce1d49671117d90ce199a820d9bc58c09a265f44
Biel Sala <bielsalamimo@gmail.com>
2023-12-27 22:56:22 +0100

    User a 'loop{}'

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,23 +6,32 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is: {}", secret_number);
 
-    let mut guess = String::new();
-    println!("Your guess: ");
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        let mut guess = String::new();
+        println!("Your guess: ");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input a numeric value");
+                continue;
+            }
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => {
-            println!("Too small");
-        }
-        Ordering::Greater => {
-            println!("Too big");
-        }
-        Ordering::Equal => {
-            println!("You win!");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => {
+                println!("Too small");
+            }
+            Ordering::Greater => {
+                println!("Too big");
+            }
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }

b6034ef12b845eb64ba3f32b5d648b9e805870d2
Biel Sala <bielsalamimo@gmail.com>
2023-12-28 13:22:40 +0100

    Guessing game: guess itself with simple (min+max) / 2 algorithm

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,37 +1,48 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {}", secret_number);
+    const MIN: u32 = 1;
+    const MAX: u32 = 100;
 
-    loop {
-        let mut guess = String::new();
-        println!("Your guess: ");
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+    let mut guess_sum = 0;
+    let mut n_loops = 0;
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please input a numeric value");
-                continue;
-            }
-        };
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => {
-                println!("Too small");
-            }
-            Ordering::Greater => {
-                println!("Too big");
-            }
-            Ordering::Equal => {
-                println!("You win!");
-                break;
+    for n in 1..=10 {
+        println!("    Running game {n}");
+        let secret_number = rand::thread_rng().gen_range(MIN..=MAX);
+        println!("        Secret number is: {secret_number}");
+        let mut min_guess = MIN;
+        let mut max_guess = MAX;
+        let mut guess_count = 0;
+        loop {
+            let guess: u32 = guess_in_range(min_guess, max_guess);
+            guess_count += 1;
+            match guess.cmp(&secret_number) {
+                Ordering::Less => {
+                    println!("        Too small");
+                    min_guess = guess;
+                }
+                Ordering::Greater => {
+                    println!("        Too big");
+                    max_guess = guess;
+                }
+                Ordering::Equal => {
+                    println!("        You win!");
+                    break;
+                }
             }
         }
+        guess_sum += guess_count;
+        println!("        Guessed {guess_count} times");
+        n_loops = n;
     }
+    let avg: u32 = guess_sum / n_loops;
+    println!("Avg guess_count: {}", avg);
+}
+
+fn guess_in_range(min: u32, max: u32) -> u32 {
+    let guess = (min + max) / 2;
+    println!("        Guessed: {guess}");
+    guess
 }
